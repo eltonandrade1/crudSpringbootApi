b6a611c18558fad02f532bf088f848eb9c61652c
elton <eltonandrade1@gmail.com>
2017-11-09 21:16:53 -0300

    ...

--- src/main/java/com/elton/springboot/config/AuthorizationServerConfig.java
+++ src/main/java/com/elton/springboot/config/AuthorizationServerConfig.java
@@ -18,6 +18,7 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 	
+	
 	@Autowired
 	private AuthenticationManager authenticationManager;
 	
