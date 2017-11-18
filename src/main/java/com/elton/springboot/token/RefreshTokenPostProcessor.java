02c7bbea3e65b5cb002744869dfe24ae02b7a08a
elton <eltonandrade1@gmail.com>
2017-11-17 22:08:06 -0300

    Iniciado implementação para adicionar o token em um cookie http.

---
+++ src/main/java/com/elton/springboot/token/RefreshTokenPostProcessor.java
@@ -0,0 +1,93 @@
+package com.elton.springboot.token;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.http.server.ServletServerHttpResponse;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+@ControllerAdvice
+public class RefreshTokenPostProcessor implements ResponseBodyAdvice<OAuth2AccessToken>{
+
+	/**
+	 * Método intercepta as requisições do tipo OAuth2AccessToken e verifica se o método é
+	 * do tipo postAccessToken para retornar true
+	 * @author elton
+	 */
+	@Override
+	public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+		return returnType.getMethod().getName().equals("postAccessToken");
+	}
+	
+	/**
+	 * Método só sera executado se o método supports retornar true, onde em seguida pega o refresh token e 
+	 * adiciona a um cookie https
+	 * @author elton
+	 */
+	@Override
+	public OAuth2AccessToken beforeBodyWrite(OAuth2AccessToken body, MethodParameter returnType,
+			MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType,
+			ServerHttpRequest request, ServerHttpResponse response) {
+		
+		//Converçao de ServerHttpRequest e ServerHttpResponse para HttpServletRequest e HttpServletResponse
+		HttpServletRequest req = ((ServletServerHttpRequest) request).getServletRequest();
+		HttpServletResponse resp = ((ServletServerHttpResponse) response).getServletResponse();
+		
+		//Cast do OAuth2AccessToken para poder remover o token do body
+		DefaultOAuth2AccessToken token = (DefaultOAuth2AccessToken) body;
+		
+		//Pegando RefreshToken da requisiçao
+		String refreshToken = body.getRefreshToken().getValue();
+		
+		//add refreshToken ao cookie
+		addRefreshTokenInCookie(refreshToken, req, resp);
+		
+		//remove token do body
+		removeRefreshTokenFromBody(token);
+		
+		return body;
+	}
+
+	/**
+	 * Método responsável por remover o refreshToken do body
+	 * @param token
+	 * @author elton
+	 */
+	private void removeRefreshTokenFromBody(DefaultOAuth2AccessToken token) {
+		token.setRefreshToken(null);
+		
+	}
+
+	/**
+	 * Método responsável por adicionar o refreshToken em um cookie 
+	 * @param refreshToken
+	 * @param req
+	 * @param resp
+	 * @author elton
+	 */
+	private void addRefreshTokenInCookie(String refreshToken, HttpServletRequest req, HttpServletResponse resp) {
+		//Criando objeto cookie
+		Cookie refreshTokenCookie = new Cookie("refreshToken", refreshToken);
+		//Cookie somente será acessivel via http
+		refreshTokenCookie.setHttpOnly(true);
+		//Utilizar https
+		refreshTokenCookie.setSecure(false); //TODO alterar para true em produção
+		//Define o local que o cookie será enviado pelo brownser
+		refreshTokenCookie.setPath(req.getContextPath() + "/oauth/token");
+		//Define o tempo em dias que o cookie vai expirar
+		refreshTokenCookie.setMaxAge(2592000); // corresponde a 30 dias
+		//Adiciona o cookie a resposta
+		resp.addCookie(refreshTokenCookie);
+	}
+
+}
