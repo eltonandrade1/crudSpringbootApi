d9afa3e75d496a4d8ccdb7c15615e0640ce9c34d
elton <eltonandrade1@gmail.com>
2017-11-21 21:04:33 -0300

    Implementado CorsFilter para requições javaScript.

---
+++ src/main/java/com/elton/springboot/cors/CorsFilter.java
@@ -0,0 +1,63 @@
+package com.elton.springboot.cors;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter implements Filter {
+
+	private String originPermitida = "http://localhost:8080"; // TODO: Configurar para diferentes ambientes
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
+			throws IOException, ServletException {
+
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) resp;
+
+		//Configura a origem permitida para requisição
+		response.setHeader("Access-Control-Allow-Origin", originPermitida);
+		//Responsavel por enviar o refreshToken armazenado no cookie
+		response.setHeader("Access-Control-Allow-Credentials", "true");
+
+		if ("OPTIONS".equals(request.getMethod()) && originPermitida.equals(request.getHeader("Origin"))) {
+			//Configura os metodos http permitidos
+			response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
+			// Configura os headers permitidos
+			response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
+			// configura o tempo que o browser faz um nova requisição para 1 hora
+			response.setHeader("Access-Control-Max-Age", "3600");
+
+			response.setStatus(HttpServletResponse.SC_OK);
+		} else {
+			chain.doFilter(req, resp);
+		}
+
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
