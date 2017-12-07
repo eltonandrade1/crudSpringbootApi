be3a67c2165f48242aed50ed9e30aac993c70398
elton <eltonandrade1@gmail.com>
2017-12-07 19:55:30 -0300

    Implementado a Atualização de lançamento e validação de pessoa

--- src/main/java/com/elton/springboot/service/LancamentoService.java
+++ src/main/java/com/elton/springboot/service/LancamentoService.java
@@ -20,46 +20,45 @@ import com.elton.springboot.repository.projection.ResumoLancamento;
 
 @Service
 public class LancamentoService {
-	
+
 	@Autowired
 	LancamentoRepository lancamentoRepository;
-	
+
 	@Autowired
 	PessoaRepository pessoaRepository;
-	
+
 	@Autowired
 	CategoriaRepository categoriaRepository;
 
 	public List<Lancamento> findAll() {
 		return lancamentoRepository.findAll();
 	}
-	
+
 	public Lancamento buscarPessoa(Long id) {
 		return getById(id);
 	}
-	
 
 	private Lancamento getById(Long id) {
 		Lancamento lancamento = lancamentoRepository.findOne(id);
-		if(lancamento != null) {
+		if (lancamento != null) {
 			return lancamento;
 		}
-		throw new EmptyResultDataAccessException(1); 
+		throw new EmptyResultDataAccessException(1);
 	}
 
 	public Lancamento salvarLancamento(Lancamento lancamento) {
 		Pessoa pessoa = pessoaRepository.findOne(lancamento.getPessoa().getId());
 		Categoria categoria = categoriaRepository.findOne(lancamento.getCategoria().getId());
-		
-		//Validação para pessoa inativa ou inexistente		
-		if(pessoa == null || pessoa.isInativo()) {
+
+		// Validação para pessoa inativa ou inexistente
+		if (pessoa == null || pessoa.isInativo()) {
 			throw new PessoaInexistenteOuInativaException();
 		}
-		//Validação para categoria inexistente
-		if(categoria == null) {
+		// Validação para categoria inexistente
+		if (categoria == null) {
 			throw new CategoriaInexistenteException();
 		}
-		
+
 		return lancamentoRepository.save(lancamento);
 	}
 
@@ -69,22 +68,42 @@ public class LancamentoService {
 
 	public void remover(Long id) {
 		lancamentoRepository.delete(id);
-		
+
 	}
-	
-	public Lancamento atualizar(Long id, Lancamento lancamento) {
-		Lancamento lancamentoSalvo = getById(id);
-		if(lancamentoSalvo != null) {
-			// Copia as propriedades do objeto pessoa para pessoaSalva ignorando o id.
-			BeanUtils.copyProperties(lancamento, lancamentoSalvo, "id");
-		} else {
-			throw new EmptyResultDataAccessException(1); 
+
+	public Lancamento atualizar(Long codigo, Lancamento lancamento) {
+		Lancamento lancamentoSalvo = buscarLancamentoExistente(codigo);
+		if (!lancamento.getPessoa().equals(lancamentoSalvo.getPessoa())) {
+			validarPessoa(lancamento);
 		}
-		return lancamentoRepository.save(lancamento);
+
+		BeanUtils.copyProperties(lancamento, lancamentoSalvo, "codigo");
+
+		return lancamentoRepository.save(lancamentoSalvo);
+	}
+
+	private void validarPessoa(Lancamento lancamento) {
+		Pessoa pessoa = null;
+		if (lancamento.getPessoa().getId() != null) {
+			pessoa = pessoaRepository.findOne(lancamento.getPessoa().getId());
+		}
+
+		if (pessoa == null || pessoa.isInativo()) {
+			throw new PessoaInexistenteOuInativaException();
+		}
+	}
+
+	private Lancamento buscarLancamentoExistente(Long codigo) {
+		Lancamento lancamentoSalvo = lancamentoRepository.findOne(codigo);
+		if (lancamentoSalvo == null) {
+			throw new IllegalArgumentException();
+		}
+		return lancamentoSalvo;
 	}
 
 	/**
 	 * Método utilizado para uma consulta resumida de lançamentos
+	 * 
 	 * @param lancamentoFilter
 	 * @return ResumoLancamento
 	 * @author elton
